67078b4887978997e0c6826b86dbc9fdca4e93c6
Digambar Kamalkar Chaudhari <digambarc@cybage.com>
2022-11-17 11:03:03 +0530

    all files has been added

---
+++ ConstructorEx/ConstructorEx1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ConstructorEx1
+{
+    using System;
+    internal class Program
+    {
+
+        //we are doing the constructor overloading
+        public Program()
+        {
+            Console.WriteLine("default const. invoke");
+        }
+
+        public Program(int a,int b,int c)
+        {
+            Console.WriteLine("Enter the Number");
+            Console.WriteLine(a + b + c); ;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Program c = new Program(1, 2, 3);
+            Console.ReadLine();
+
+        }
+    }
+}
